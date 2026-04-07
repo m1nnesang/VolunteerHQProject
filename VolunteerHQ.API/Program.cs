70f58f17083c3ec5bb69b1f4e93482fdf1638185
m1nnesang <vladchereshev@gmail.comgit config --global user.name Vladgit statusgit config --global user.email vladchereshev@gmail.com>
2026-04-07 18:08:08 +0200

    added DbContext

--- VolunteerHQ.API/Program.cs
+++ VolunteerHQ.API/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using VolunteerHQ.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
